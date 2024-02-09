03a96a7d14453a754a5db7044f82b19bfa706c0b
unknown <ruchellguilherme100@gmail.com>
2024-02-09 16:25:56 -0300

    setando package

--- src/main/java/com/clinica/veterinaria/api/ApiApplication.java
+++ src/main/java/com/clinica/veterinaria/api/ApiApplication.java
@@ -1,4 +1,4 @@
-package com.clinicaveterinaria.api;
+package com.clinica.veterinaria.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
