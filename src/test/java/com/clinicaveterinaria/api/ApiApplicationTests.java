5d5d81ed844fdf7fb1ccd2438393553360d999d7
unknown <ruchellguilherme100@gmail.com>
2024-02-09 16:24:26 -0300

    criando estrutura inicial do projeto

---
+++ src/test/java/com/clinicaveterinaria/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicaveterinaria.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
